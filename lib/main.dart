6798a6ade0540aad8cb7327abe4d3f004a66b31b
Arpit Kushwaha <arpitkushwah01@gmail.com>
2021-02-03 00:04:23 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Container(
+            color: Colors.blue,
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Container(
+            color: Colors.pink,
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Container(
+          color: Colors.yellow,
+        );
+      },
+    );
+  }
+}
